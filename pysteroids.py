6c157d33cf2b6ad8cce185d4dea7d24861b76eda
CodeConnoisseur74 <sherry.andrews@outlook.com>
2022-10-06 23:39:26 +0200

    add ship image to game

--- pysteroids.py
+++ pysteroids.py
@@ -11,10 +11,10 @@ pygame.init()
 fps = pygame.time.Clock()
 
 # colors
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLACK = (0 ,0 ,0)
 
 # globals
 WIDTH = 800
@@ -28,10 +28,10 @@ pygame.display.set_caption("Pysteroids")
 # load images
 bg = pygame.image.load(os.path.join("images", "bg.jpg"))
 debris = pygame.image.load(os.path.join("images", "debris2_brown.png"))
+ship = pygame.image.load(os.path.join("images", "ship.png"))
+
 
 # draw game function
-
-
 def draw(canvas):
     global time
     canvas.fill(BLACK)
@@ -39,6 +39,26 @@ def draw(canvas):
     canvas.blit(debris, (time * 0.3, 0))
     canvas.blit(debris, (time * 0.3 - WIDTH, 0))
     time = time + 1
+    canvas.blit(ship, (WIDTH/2 -50,HEIGHT/2 -50))
+
+
+# (0,0)
+# (width,0)
+# (0,height)
+# (width,height)
+
+
+# handle input function
+def handle_input():
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+def update_screen():
+    pygame.display.update()
+    fps.tick(60)
 
 
 # pysteroids game loop
